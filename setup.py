ac1cd2f47183e4729f1db55d879ec69484a2a121
Nick Murphy <nick.murphy@devo.com>
2018-12-20 12:31:16 -0500

    setup.py and directory structure to work with it

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='devodstoolkit',
+    version='0.1.0',
+    author='Nick Murphy',
+    author_email='nick.murphy@devo.com',
+    description='APIs for querying and loading data into Devo',
+    url='https://github.com/devods/devodstoolkit',
+    python_requires='>=3',
+    packages=find_packages()
+)
